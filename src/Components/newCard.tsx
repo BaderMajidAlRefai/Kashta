4cdb72c4780758dbd90beec9ffd1f6fd05771315
Bader Al Refai <badermajidalrefai@gmail.com>
2026-05-29 22:55:49 +0300

    added card for creating new trips

---
+++ src/Components/newCard.tsx
@@ -0,0 +1,13 @@
+function NewCard(){
+    return(
+        <div className="border-2 border-white/70  rounded-2xl w-75 h-65 flex items-center justify-center">
+            <div className="border-2 border-white h-30 w-30 rounded-[100%] flex hover:bg-[#182840]/60 cursor-pointer duration-75">
+                <div className="justify-center items-center flex flex-1">
+                    <h1 className="text-8xl font-thin text-white">+</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NewCard
