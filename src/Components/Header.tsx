c83e2d6624a65f6ac0864537e61769087f4ecaea
Bader Al Refai <badermajidalrefai@gmail.com>
2026-05-29 23:23:40 +0300

    The logo now isn't selectable

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,7 +4,7 @@ function Header(){
         <header>
             <nav className="bg-[#102038]">
                 <ul className="flex items-center gap-10 h-12 ml-10">
-                    <h1 className="text-2xl text-white"> Kashta </h1>
+                    <h1 className="text-2xl text-white select-none"> Kashta </h1>
                     <a href="#"><h2 className="text-white">Home</h2></a>
                     <a href="#"><h2 className="text-white">About Us</h2></a>
                     <a href="#"><h2 className="text-white">Settings</h2></a>
