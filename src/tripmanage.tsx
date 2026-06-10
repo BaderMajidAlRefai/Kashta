e25f3fef58e37d44245b31b8e24d2d05829ca0aa
Bader Al Refai <badermajidalrefai@gmail.com>
2026-06-10 15:48:04 +0300

    Added return and name

--- src/tripmanage.tsx
+++ src/tripmanage.tsx
@@ -2,7 +2,7 @@ import Return from './assets/return.png'
 import { useState } from 'react'
 import TripManageBox from './Components/tripmanagebox.tsx'
 import type { Trip } from './types'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 
 type TripManageProps = {
@@ -23,11 +23,12 @@ function TripManage({trips, setTrips}: TripManageProps){
         <div className="flex flex-1 items-center justify-center">
             <div className="bg-[#182840]/90 blur-x w-3/4 h-3/4 rounded-2xl flex flex-col p-5 gap-4">
                 <div>
-                    <img src={Return} className='w-5'></img>
+                    <Link to={"/"}><img src={Return} className='w-5'></img></Link>
                 </div>
                 <div className='p-5 flex flex-col gap-5'>
                     <div className='flex flex-col gap-5'>
-                        <h1 className="text-white text-5xl">{trip.location.name ?? "No Location"}</h1>
+                        <h1 className="text-white text-5xl">{trip.name ?? "No Location"}</h1>
+                        <h2 className="text-white text-3xl">{trip.location.name ?? "No Location"}</h2>
                         <h2 className="text-white text-3xl">{trip.date ?? "No date"}</h2>
                     </div>
                     <div className="flex justify-center gap-20 h-15">
